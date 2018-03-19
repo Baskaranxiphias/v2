036058351c78adf47934d8a9eb7c7e335307e179
Baskaranxiphias <baskaran@xiphiastec.com>
2018-03-19 16:10:55 +0530

    about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var skrollr: any;
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+        var mySkrollr = skrollr.init({
+            forceHeight: false,
+            easings: {
+                easeOutBack: function (p, s) {
+                    s = 1.70158;
+                    p = p - 1;
+                    return (p * p * ((s + 1) * p + s) + 1);
+                }
+            },
+            mobileCheck: function () {
+                //hack - forces mobile version to be off
+                return false;
+            }
+        });
+
+
+        $("#flip").click(function () {
+            $("#panel").slideToggle("slow");
+        });
+    }
 
 }
